057e07c79e79f4ab25618ca6848e4848279f63be
Hunter Parcells <hparcells@icloud.com>
2020-05-16 12:16:10 -0400

    Meta

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 
 import App from './components/App/App';
 
 import './styles/global.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+// Tracking
+if (process.env.NODE_ENV === 'production') {
+  ReactGA.initialize('UA-134185568-9');
+  ReactGA.pageview('/');
+}
