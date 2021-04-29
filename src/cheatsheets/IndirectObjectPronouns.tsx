388e71bcf6cdfc14cd6b2c6785bff328a605a8d7
Hunter Parcells <hparcells@icloud.com>
2021-04-29 07:39:51 -0400

    Remove Unused

--- src/cheatsheets/IndirectObjectPronouns.tsx
+++ src/cheatsheets/IndirectObjectPronouns.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
 import CheatSheet from '../components/CheatSheet/CheatSheet';
-import ConjugationTable from '../components/ConjugationTable/ConjugationTable';
-import ConjugationTableWrapper from '../components/ConjugationTableWrapper';
 
 function IndirectObjectPronouns() {
   return (
