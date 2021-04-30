889ec24f7cb51f8525cb0330e74a1022fe1d9234
Hunter Parcells <hparcells@icloud.com>
2021-04-30 07:50:34 -0400

    Indirect Object Pronouns

--- src/cheatsheets/IndirectObjectPronouns.tsx
+++ src/cheatsheets/IndirectObjectPronouns.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
 
 import CheatSheet from '../components/CheatSheet/CheatSheet';
+import ConjugationTable from '../components/ConjugationTable/ConjugationTable';
+import ConjugationTableWrapper from '../components/ConjugationTableWrapper';
 
 function IndirectObjectPronouns() {
   return (
     <CheatSheet title='Indirect Object Pronouns'>
-      <p>Coming soon.</p>
+      <p>
+        The <u>Indirect Object</u> tells us where the direct object is going. The indirect object
+        answers the question "to whom/for whom" the action of the verb is performed.
+      </p>
+      <p>
+        <u>Indirect Object Pronouns</u> take the place of the indirect object. The indirect object
+        pronoun comes <strong>before the conjugated verb</strong>.
+      </p>
+
+      <ConjugationTableWrapper>
+        <ConjugationTable
+          title='Indirect Object Pronouns'
+          yo='me'
+          tu='te'
+          ud='le'
+          nos='nos'
+          vos='os'
+          uds='les'
+        />
+      </ConjugationTableWrapper>
     </CheatSheet>
   );
 }
